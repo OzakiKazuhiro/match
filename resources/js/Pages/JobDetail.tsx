0f35c703f979afed02dd195f5e43b1ae30a3ffaf
OzakiKazuhiro <sciencegreen1110@gmail.com>
2025-05-04 03:44:59 +0900

    横並び修正

--- resources/js/Pages/JobDetail.tsx
+++ resources/js/Pages/JobDetail.tsx
@@ -897,6 +897,7 @@ export default function JobDetail({
                                                     height="24"
                                                     viewBox="0 0 24 24"
                                                     fill="currentColor"
+                                                    className="p-job-detail__share-icon"
                                                 >
                                                     <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
                                                 </svg>
@@ -917,14 +918,15 @@ export default function JobDetail({
                                             <div className="p-job-detail__share-button-content">
                                                 <svg
                                                     xmlns="http://www.w3.org/2000/svg"
-                                                    width="24"
-                                                    height="24"
+                                                    width="20"
+                                                    height="20"
                                                     viewBox="0 0 24 24"
                                                     fill="none"
                                                     stroke="currentColor"
                                                     strokeWidth="2"
                                                     strokeLinecap="round"
                                                     strokeLinejoin="round"
+                                                    className="p-job-detail__share-icon"
                                                 >
                                                     <rect
                                                         x="9"
