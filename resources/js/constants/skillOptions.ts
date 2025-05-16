733903740081533979ef3af2d73381cf7b1ca077
OzakiKazuhiro <sciencegreen1110@gmail.com>
2025-05-16 16:01:00 +0900

    スキル追加

--- resources/js/constants/skillOptions.ts
+++ resources/js/constants/skillOptions.ts
@@ -1,27 +1,80 @@
 export const SKILL_OPTIONS = [
+    // フロントエンド
     "HTML/CSS",
     "JavaScript",
     "TypeScript",
+    "React",
+    "Vue.js",
+    "Next.js",
+    "Nuxt.js",
+    "Svelte",
+    "Angular",
+    "Tailwind CSS",
+    "Sass/SCSS",
+    "Webpack",
+    "Vite",
+
+    // バックエンド
     "PHP",
     "Laravel",
-    "Vue.js",
-    "React",
     "Node.js",
+    "Express",
     "Python",
     "Django",
+    "Flask",
+    "FastAPI",
     "Ruby",
     "Ruby on Rails",
     "Java",
     "Spring",
+    "Kotlin",
     "C#",
     ".NET",
-    "AWS",
-    "GCP",
-    "Azure",
+    "Go",
+
+    // データベース
     "MySQL",
     "PostgreSQL",
     "MongoDB",
+    "Redis",
+    "Firebase",
+    "Supabase",
+
+    // クラウド/インフラ
+    "AWS",
+    "GCP",
+    "Azure",
+    "Docker",
+    "Kubernetes",
+    "Terraform",
+    "CI/CD",
+    "Git",
+
+    // モバイル
+    "React Native",
+    "Flutter",
+    "Swift",
+    "Kotlin (Android)",
+
+    // CMS/EC
     "WordPress",
     "Shopify",
+    "WooCommerce",
+
+    // デザインツール
+    "Figma",
+    "Adobe XD",
+    "Photoshop",
+    "Illustrator",
+    "UI/UXデザイン",
+
+    // その他
+    "GraphQL",
+    "REST API",
+    "WebSocket",
+    "WebRTC",
+    "機械学習",
+    "AI",
+    "セキュリティ",
     "その他",
 ];
